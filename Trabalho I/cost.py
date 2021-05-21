0d252692df1610232683ce75861c3c68111c7442
jbfneves <37002950+jbfneves@users.noreply.github.com>
2021-05-21 12:52:53 +0100

    é esta

--- Trabalho I/cost.py
+++ Trabalho I/cost.py
@@ -12,6 +12,7 @@ cursor_psql = conn.cursor()
 
 def cost(id):
     #print("TaxiId: " + str(id))
+    #sql = "SELECT N, ST_Distance(ST_Pointn(proj_track,N), ST_PointN(proj_track, n+1)) as D from tracks, (SELECT generate_series(1,ST_NumPoints(proj_track)-1) as N from tracks where id=" + str(id) + ") as X where id=" + str(id);
     sql = "SELECT ts,proj_track FROM tracks WHERE id="+str(id)
     cursor_psql.execute(sql)
     results = cursor_psql.fetchall()
@@ -20,42 +21,54 @@ def cost(id):
     ts = results[0][0]
     dt_object = datetime.fromtimestamp(ts)
     print(dt_object)
-    if (dt_object.hour>9 and dt_object.hour<21):
-        #print("DIA")
+    if (dt_object.hour>6 and dt_object.hour<21):
+        print("DIA")
         distance_fare = 212.77
+        bandeirada=1800
+        total_money = 3.25
     else:
-        #print("NOITE")
+        print("NOITE")
         distance_fare = 178.57
+        bandeirada=1440
+        total_money = 3.9
+    #print(distance_fare)
     seconds = 0
     total_time=0
     total_distance=0
-    total_money = 0.0
     distance = 0
-    first = 1
+    flag_first = 1
+    flag_bandeirada = 1
     for (x,y) in xy:
         seconds+=1
         total_time+=1
-        if(first == 1):
+        if(flag_first == 1):
             prevX = x
             prevY = y
-            first = 0
+            flag_first = 0
         else:
             distance_aux = math.sqrt(abs(x-prevX)**2+abs(y-prevY)**2)
+            #print("      Dist: " + str(distance_aux))
             if(distance_aux<50):
                 distance += distance_aux
                 total_distance += distance_aux
                 prevX=x
                 prevY=y
 
-        if(seconds==24):
-            total_money+=0.1
-            seconds=0
-            distance=0
-            continue
-        if (distance >= distance_fare):
-            total_money+=0.1
-            seconds=0
-            distance-=distance_fare
+        if(flag_bandeirada == 1):
+            if distance >= bandeirada:
+                flag_bandeirada = 0
+                seconds = 0
+                distance -= bandeirada
+        else:
+            if(seconds==24):
+                total_money+=0.1
+                seconds=0
+                distance=0
+                continue
+            if (distance >= distance_fare):
+                total_money+=0.1
+                seconds=0
+                distance-=distance_fare
     
     print("Custo: " + str(round(total_money,2)))
     print("Distancia: " + str(round(total_distance/1000,2)))
@@ -75,5 +88,13 @@ cost(12974)
 print()
 cost(12985)
 print() """
+
+def concelho():
+    sql = "SELECT concelho, tracks.id from tracks, cont_aad_caop2018 WHERE state='BUSY' AND ST_Within(ST_StartPoint(proj_track), proj_boundary) order by concelho"
+    cursor_psql.execute(sql)
+    results = cursor_psql.fetchall()
+
+    print(results)
+
 
 
--- concelhos.py
+++ concelhos.py
@@ -11,25 +11,25 @@ def polygon_to_points(polygon_string):
         ys.append(float(y))
     return xs,ys
 
-scale=1/300000
+scale=1/30000
 conn = psycopg2.connect("dbname=joaoneves user=joaoneves")
 cursor_psql = conn.cursor()
 
 # Calculate figure size
-sql ="select st_astext(st_envelope(st_collect(st_simplify(proj_boundary,100,FALSE)))) from cont_aad_caop2018"
+sql ="select st_astext(st_envelope(st_collect(st_simplify(proj_boundary,100,FALSE)))) from cont_aad_caop2018 where concelho='PORTO'"
 cursor_psql.execute(sql)
 results = cursor_psql.fetchall()
 row = results[0]
 polygon_string = row[0]
 xs,ys = polygon_to_points(polygon_string)
 
-
 width_in_inches = ((max(xs)-min(xs))/0.0254)*1.1
 height_in_inches = ((max(ys)-min(ys))/0.0254)*1.1
 
 fig = plt.figure(figsize=(width_in_inches*scale,height_in_inches*scale))
 
-sql = "select st_astext(st_simplify(proj_boundary,100,False)) from cont_aad_caop2018"
+#É ESTAAAAAAA
+sql = "SELECT st_astext(st_union(proj_boundary)) from cont_aad_caop2018 where distrito in ('PORTO') group by concelho"
 cursor_psql.execute(sql)
 results = cursor_psql.fetchall()
 #print(results[0])
@@ -38,4 +38,10 @@ for row in results:
     polygon_string = row[0]
     xs, ys = polygon_to_points(polygon_string)
     plt.plot(xs,ys,color='red')
+
+""" sql = "SELECT concelho, st_astext(st_union(proj_boundary)) from cont_aad_caop2018 where distrito in ('PORTO') group by concelho"
+cursor_psql.execute(sql)
+results = cursor_psql.fetchall()
+print(results) """
+
 plt.show()
